41c996b1c39deab5fe56a5380d9214e8fa6baf83
Pettric H. Mamea <103038493+Beedrick@users.noreply.github.com>
2023-12-16 21:26:22 +1300

    added catagory buttons, task count, styled task box + done button

--- script.js
+++ script.js
@@ -3,15 +3,35 @@ function addToList() {
     newDiv.className = 'taskBox';
 
     var removebutton = document.createElement('button');
-    removebutton.textContent = 'Remove';
-    removebutton.className = 'remove-button';
+    removebutton.className = 'doneButton';
+    removebutton.textContent = 'Done';
 
     removebutton.addEventListener('click', function() {
         newDiv.remove();
+        decNumTask();
     });
+
+    if(document.getElementById("numOfTask").innerHTML == 10){
+        prompt("Too many tasks!")
+        prompt().
+        console.log("Error: no more than 10 tasks allowed!")
+        return;
+    }
 
     newDiv.appendChild(removebutton);
 
     document.getElementById('container').appendChild(newDiv);
+    incNumTask();
+}
 
+function incNumTask() {
+    document.getElementById("numOfTask").innerHTML++;
+}
+
+function decNumTask() {
+    document.getElementById("numOfTask").innerHTML--;
+}
+
+function catagory(){
+    
 }
