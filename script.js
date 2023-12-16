d928ec4cf2c8fcb09da1039ff5274ea64e7e794d
Pettric H. Mamea <103038493+Beedrick@users.noreply.github.com>
2023-12-17 10:07:04 +1300

    styling changed body background, input text box added

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function addToList() {
+function addToList(catagory) {
     var newDiv = document.createElement('div');
     newDiv.className = 'taskBox';
+
+    var cataImage = document.createElement('img');
+    cataImage.src = catagory;
+    cataImage.className = 'taskImage';
+
+    newDiv.appendChild(cataImage);
 
     var removebutton = document.createElement('button');
     removebutton.className = 'doneButton';
@@ -11,15 +17,16 @@ function addToList() {
         decNumTask();
     });
 
+    var textBox = document.createElement('input');
+    textBox.className = 'inputBox';
+
     if(document.getElementById("numOfTask").innerHTML == 10){
-        prompt("Too many tasks!")
-        prompt().
         console.log("Error: no more than 10 tasks allowed!")
         return;
     }
 
     newDiv.appendChild(removebutton);
-
+    newDiv.appendChild(textBox);
     document.getElementById('container').appendChild(newDiv);
     incNumTask();
 }
@@ -30,8 +37,4 @@ function incNumTask() {
 
 function decNumTask() {
     document.getElementById("numOfTask").innerHTML--;
-}
-
-function catagory(){
-    
 }
