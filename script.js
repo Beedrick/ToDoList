61795fa35005aab405ef4da9699e11907f7006e5
Pettric H. Mamea <103038493+Beedrick@users.noreply.github.com>
2023-12-26 15:24:27 +1300

    added an edit function/button, All task boxes held in the id='container' div that are appended using the add task button changed from divs to input elements so that editing functionality implementation was easier, little bit of css work done, added sounds for edit/save button

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ function addToList() {
     var newDiv = document.createElement('div');
     newDiv.className = 'taskBox';
 
+    var taskInput = document.createElement('input');
+    taskInput.type = 'text';
+    taskInput.readOnly = 'true';
+    taskInput.id = 'inputBox';
+
     var userInBox = document.getElementById('userInputDiv');
 
     var imagesource = userInBox.lastChild.src;
@@ -22,16 +27,38 @@ function addToList() {
     removebutton.className = 'doneButton';
     removebutton.textContent = 'Done';
 
+    var editButton = document.createElement('button');
+    editButton.className = 'doneButton';
+    editButton.textContent = 'edit';
+
     removebutton.addEventListener('click', function() {
         newDiv.remove();
         decNumTask();
 
         var completeTask = new Audio('/audio/TaskDone.MP3');
+        completeTask.volume = 1;
         completeTask.play();
     });
 
-    var textBox = document.createElement('div');
-    textBox.id = 'inputBox';
+    editButton.addEventListener('click', function(){
+        
+        if(editButton.innerText.toLowerCase() == 'edit'){
+            var editSound = new Audio('/audio/editSoundF.MP3');
+            editSound.play();
+            editSound.volume = 0.15;
+            taskInput.removeAttribute('readonly');
+            taskInput.focus();
+            editButton.innerText = 'save';
+        }else {
+            taskInput.readOnly = 'true';
+            editButton.innerText = 'edit';
+            var saveSound = new Audio('/audio/SaveSound.MP3');
+            saveSound.volume = 0.15;
+            saveSound.play();
+        }
+
+    })
+
 
     if(document.getElementById("numOfTask").innerHTML == 10){
         console.log("Error: no more than 10 tasks allowed!")
@@ -40,14 +67,16 @@ function addToList() {
 
     var userIn = document.getElementById('user_input');
     let text = userIn.value;
-    textBox.innerText = text;
+    taskInput.value = text;
 
     var writeTask = new Audio('/audio/WriteTask.MP3');
     writeTask.play();
 
     newDiv.appendChild(cataImage);
-    newDiv.appendChild(textBox);
+    //newDiv.appendChild(textBox);
+    newDiv.appendChild(taskInput);
     newDiv.appendChild(removebutton);
+    newDiv.appendChild(editButton);
 
     document.getElementById('container').appendChild(newDiv);
     
